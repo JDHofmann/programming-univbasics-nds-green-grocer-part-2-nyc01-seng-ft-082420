259f159cfbbe47b8f0bed731e3550047d432b4d1
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-21 14:19:16 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -8,9 +8,9 @@ def apply_coupons(cart, coupons)
 #   {:item => "KALE", :price => 3.00, :clearance => false, :count => 1},
 #   {:item => "AVOCADO W/COUPON", :price => 2.50, :clearance => true, :count => 2}
 # ]
-coupons.each do | coupon_item |
+coupons.each do | coupon_i |
   # find coupon item in cart
-  find_item_by_name_in_collection(coupon_item[:item], cart)
+  discounted_item = find_item_by_name_in_collection(coupon_i[:item], cart)
 end
 
 end
