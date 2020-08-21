87d0d7c7795bc22d5ebef42857f462ba4396e593
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-21 14:35:02 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -12,8 +12,9 @@ def apply_coupons(cart, coupons)
     # find coupon item in cart
     discounted_item = find_item_by_name_in_collection(coupon_i[:item], cart)
     discounted_item_cost = coupon_i[:cost]/coupon_i[:num]
+    
     new_item = {
-      
+
     }
   end
 cart
