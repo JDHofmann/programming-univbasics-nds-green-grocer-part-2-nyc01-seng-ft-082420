4a37a54373e65e8c811d4091e5001e8192baa0e9
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-21 16:53:44 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -47,6 +47,6 @@ def checkout(cart, coupons)
   applied_coupons = apply_coupons(consolidated_cart, coupons)
   applied_clearance = apply_clearance(applied_coupons)
 
-
+  total = 
   #return float total cart
 end
