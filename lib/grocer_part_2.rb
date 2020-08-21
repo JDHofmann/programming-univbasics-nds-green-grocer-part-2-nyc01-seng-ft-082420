96237eaacef8a4c62176240deba8e2d153cab473
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-21 16:45:45 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -37,7 +37,7 @@ def apply_clearance(cart)
     if cart_item[:clearance]
       cart_item[:price] = cart_item[:price]*0.8
       cart_item[:price] = cart_item[:price].round(2)
-      binding.pry
+      # binding.pry
     end
   end
 
